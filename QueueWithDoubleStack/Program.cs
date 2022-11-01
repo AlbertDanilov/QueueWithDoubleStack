12f824475c0daa70e1648460c699770649c8a60d
Danilov <albert95danilov@gmail.com>
2022-11-01 21:01:19 +0300

    Добавьте файлы проекта.

---
+++ QueueWithDoubleStack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueWithDoubleStack
+{
+    public class Queue<T> {
+        private Stack<T> stack_pop = new Stack<T>();
+        private Stack<T> stack_push = new Stack<T>();
+
+        public void addToQueue(T element) {
+            stack_push.Push(element);
+        }
+
+        public T getFromQueue()
+        {
+            if (stack_pop.Count == 0) { 
+                while (stack_push.Count > 0) {
+                    stack_pop.Push(stack_push.Pop());
+                }
+            }
+
+            if (stack_pop.Count == 0)
+            {
+                return default(T);
+            }
+
+            return stack_pop.Pop();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            numbers.addToQueue(1);
+            numbers.addToQueue(2);
+            numbers.addToQueue(3);
+
+            numbers.getFromQueue();
+
+            numbers.addToQueue(4);
+            numbers.addToQueue(5);
+
+            numbers.getFromQueue();
+            numbers.getFromQueue();
+            numbers.getFromQueue();
+            numbers.getFromQueue();
+            numbers.getFromQueue();
+            numbers.getFromQueue();
+        }
+    }
+}
